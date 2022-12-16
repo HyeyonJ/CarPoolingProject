7dcec151cbdc3863461ee5b4104d3b2575a39e5c
HyeyeonJ <michaelvia29@gmail.com>
2022-12-16 15:03:48 +0900

    first commit

---
+++ src/main/java/project/carPooling/CarPoolingApplication.java
@@ -0,0 +1,15 @@
+package project.carPooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPoolingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPoolingApplication.class, args);
+		
+		System.out.println("이렇게");
+	}
+
+}
---
+++ src/test/java/project/carPooling/CarPoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.carPooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPoolingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
