93228f4f98075b3824a140d7e834e2326b948d48
HyeyeonJ <michaelvia29@gmail.com>
2022-12-20 12:46:31 +0900

    driver 네이버 api 화면 구성

--- src/main/java/project/carPooling/driver/controller/homeController.java
+++ src/main/java/project/carPooling/driver/controller/homeController.java
@@ -1,5 +1,13 @@
 package project.carPooling.driver.controller;
 
-public class config {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class homeController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
 }
