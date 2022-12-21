836778a6b2e74fb71861bb5015e3d0a7541a75de
HyeyeonJ <michaelvia29@gmail.com>
2022-12-21 10:42:34 +0900

    homeController 와 home view 연결

--- src/main/java/project/carPooling/driver/controller/loginController/DrvHomeController.java
+++ src/main/java/project/carPooling/driver/controller/loginController/DrvHomeController.java
@@ -1,4 +1,4 @@
-package project.carPooling.driver.controller;
+package project.carPooling.driver.controller.loginController;
 
 import java.util.Enumeration;
 
@@ -17,37 +17,37 @@ import project.carPooling.global.session.SessionVar;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-public class HomeController {
-	
-	@GetMapping("/")
-	public String home(Model model
-					, HttpServletRequest req) {
-		
-		HttpSession session = req.getSession(false);
-		if(session == null) { return "home"; }
-		
-		//이 위치에 session이 존재함
-//		for(String sName : session.getAttributeNames()..asIterator())
-		Enumeration<String> sessionName = session.getAttributeNames();
-		while(sessionName.hasMoreElements()) {
-			String name = sessionName.nextElement();
-			log.info("session {}, {}", name, session.getAttribute(name));
-		}
-		
-		log.info("{}, {}, {}, {}, {}"
-				, session.getId()
-				, session.getMaxInactiveInterval()
-				, session.getCreationTime()
-				, session.getLastAccessedTime()
-				, session.isNew());
-		
-		//sessionManager에서 넘어온 Object가 driver 혹은 passenger인걸 알 수 있으므로 강제 형변환해주기
-		DriverInfo driver = (DriverInfo)session.getAttribute(SessionVar.LOGIN_DRIVER);
-		
-		if(driver == null) { return "home"; }
-		
-		model.addAttribute("driver", driver);
-		
-		return "driver/home/dLoginHome";
-	}
+public class DrvHomeController {
+//	
+//	@GetMapping("/")
+//	public String home(Model model
+//					, HttpServletRequest req) {
+//		
+//		HttpSession session = req.getSession(false);
+//		if(session == null) { return "home"; }
+//		
+//		//이 위치에 session이 존재함
+////		for(String sName : session.getAttributeNames()..asIterator())
+//		Enumeration<String> sessionName = session.getAttributeNames();
+//		while(sessionName.hasMoreElements()) {
+//			String name = sessionName.nextElement();
+//			log.info("session {}, {}", name, session.getAttribute(name));
+//		}
+//		
+//		log.info("{}, {}, {}, {}, {}"
+//				, session.getId()
+//				, session.getMaxInactiveInterval()
+//				, session.getCreationTime()
+//				, session.getLastAccessedTime()
+//				, session.isNew());
+//		
+//		//sessionManager에서 넘어온 Object가 driver 혹은 passenger인걸 알 수 있으므로 강제 형변환해주기
+//		DriverInfo driver = (DriverInfo)session.getAttribute(SessionVar.LOGIN_DRIVER);
+//		
+//		if(driver == null) { return "home"; }
+//		
+//		model.addAttribute("driver", driver);
+//		
+//		return "driver/home/dLoginHome";
+//	}
 }
