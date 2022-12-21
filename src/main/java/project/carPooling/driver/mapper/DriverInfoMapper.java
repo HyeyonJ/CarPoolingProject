77086f945e94bc4c9a7df057f123b4275eb2d861
DYon <dancingfrog.lee@gmail.com>
2022-12-21 09:50:57 +0900

    DriverMapper,Repository 내용 추가 PassengerAppConfig 수정

--- src/main/java/project/carPooling/driver/mapper/DriverInfoMapper.java
+++ src/main/java/project/carPooling/driver/mapper/DriverInfoMapper.java
@@ -1,8 +1,22 @@
 package project.carPooling.driver.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import project.carPooling.driver.domain.DriverInfo;
 
 @Mapper
 public interface DriverInfoMapper {
 
+	public Integer insert(DriverInfo driverInfo);
+	
+	public DriverInfo selectById (int id);
+	
+	public DriverInfo selectByLoginId (String loginId);
+	
+	public List<DriverInfo> selectAll();
+	
+	public void deleteAll();
+	
 }
--- src/main/java/project/carPooling/driver/repository/MybatisDriverInfoRepository.java
+++ src/main/java/project/carPooling/driver/repository/MybatisDriverInfoRepository.java
@@ -1,5 +1,49 @@
 package project.carPooling.driver.repository;
 
-public class MybatisDriverInfoRepository {
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import project.carPooling.driver.domain.DriverInfo;
+import project.carPooling.driver.mapper.DriverInfoMapper;
+
+@Slf4j
+@Repository
+@RequiredArgsConstructor	//Mapper 인터페이스 호출하기 위함
+public class MybatisDriverInfoRepository implements DriverInfoRepository {
+
+	//Mapper 인터페이스 호출
+	private final DriverInfoMapper driverMapper;
+	
+	@Override
+	public DriverInfo insert(DriverInfo driverInfo) {
+		driverMapper.insert(driverInfo);
+		return driverInfo;
+	}
+
+	@Override
+	public DriverInfo selectById(int id) {
+		DriverInfo driverInfo = driverMapper.selectById(id);
+		return driverInfo;
+	}
+
+	@Override
+	public DriverInfo selectByLoginId(String loginId) {
+		DriverInfo driverInfo = driverMapper.selectByLoginId(loginId);
+		return driverInfo;
+	}
+
+	@Override
+	public List<DriverInfo> selectAll() {
+		List<DriverInfo> drivers = driverMapper.selectAll();
+		return drivers;
+	}
+
+	@Override
+	public void deleteAll() {
+		driverMapper.deleteAll();
+	}
 
 }
