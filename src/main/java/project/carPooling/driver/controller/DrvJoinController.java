390c4466c4ccc18887450d4235ce7bb3a532167c
HyeyeonJ <michaelvia29@gmail.com>
2022-12-23 10:19:31 +0900

    drv 회원가입 DB 연결 repository , xml 파일 수정

--- src/main/java/project/carPooling/driver/controller/DrvJoinController.java
+++ src/main/java/project/carPooling/driver/controller/DrvJoinController.java
@@ -2,12 +2,18 @@ package project.carPooling.driver.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import project.carPooling.driver.domain.DriverInfo;
+import project.carPooling.driver.repository.MybatisDriverInfoRepository;
+import project.carPooling.driver.validation.DriverValidator;
 
 @Slf4j
 @Controller
@@ -15,20 +21,61 @@ import project.carPooling.driver.domain.DriverInfo;
 @RequestMapping("/driver")
 public class DrvJoinController {
 	
+	// 나중에 할 일
+//	private final ListMemberRepository memberRepository;
+//	private final MybatisMemberRepository memberRepository;
+	
+	private final MybatisDriverInfoRepository mybatisDriverInfoRepository;
+	private final DriverValidator driverValidator;
+	
 	@GetMapping("/join")
-	public String newMember(Model model) {
+	public String JoinMain (Model model) {
 		DriverInfo driverInfo = new DriverInfo();
 		model.addAttribute(driverInfo);
 		
-		return "driver/join/dJoin";
+		return "driver/join/dJoinMain";
+	}
+
+	@PostMapping("/join")
+	public String newMemberInsert(@ModelAttribute DriverInfo driverInfo
+			, BindingResult bindingResult) {
+		log.info("member = {}", driverInfo);
+		
+		driverValidator.validate(driverInfo, bindingResult);
+		
+		if(bindingResult.hasErrors()) {
+			return "driver/join/dJoinMain";
+		}
+		
+		mybatisDriverInfoRepository.insert(driverInfo);
+		return "redirect:/";
 	}
 	
+	@GetMapping("/join/general")
+	public String JoinGeneral (Model model) {
+		DriverInfo driverInfo = new DriverInfo();
+		model.addAttribute(driverInfo);
+		
+		return "driver/join/dJoinGeneral";
+	}
+	
+	
 	@GetMapping("/join/naver/add")
-	public String newMemberAdd(Model model) {
+	public String JoinAdd(Model model) {
 		DriverInfo driverInfo = new DriverInfo();
 		model.addAttribute(driverInfo);
 		
 		return "driver/join/dJoinAdd";
 	}
-
+	
+//	@PostConstruct
+//	public void init() {
+//		DriverInfo driverInfo = new DriverInfo();
+//		driverInfo.setUserId("admin");
+//		driverInfo.setUserPw("admin");
+//		driverInfo.setUserName("관리자");
+//
+//		mybatisDriverInfoRepository.insert(driverInfo);
+//	}
+	
 }
