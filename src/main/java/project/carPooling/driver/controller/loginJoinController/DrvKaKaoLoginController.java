f0ca6cd8dca68d66584ecc470401c59aa06175e6
HyeyeonJ <michaelvia29@gmail.com>
2022-12-27 19:40:14 +0900

    패키지명 변경

--- src/main/java/project/carPooling/driver/controller/loginJoinController/DrvKaKaoLoginController.java
+++ src/main/java/project/carPooling/driver/controller/loginJoinController/DrvKaKaoLoginController.java
@@ -1,4 +1,4 @@
-package project.carPooling.driver.controller.loginController;
+package project.carPooling.driver.controller.loginJoinController;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
