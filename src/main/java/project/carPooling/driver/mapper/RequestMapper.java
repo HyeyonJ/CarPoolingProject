3ded6bbfc2da3ec45349dee48b0a25d4023094a0
seunghyeon <ksh990913@naver.com>
2022-12-28 00:54:17 +0900

    운전자 카풀요청 리스트 기능 구현

--- src/main/java/project/carPooling/driver/mapper/RequestMapper.java
+++ src/main/java/project/carPooling/driver/mapper/RequestMapper.java
@@ -1,9 +1,14 @@
 package project.carPooling.driver.mapper;
 
+import java.util.List;
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 @Mapper
-public interface AcceptMapper {
+public interface RequestMapper {
 	public void update(@Param("rIdx")Integer rIdx);
+	
+	public List<Map<String, Object>> selectRequestByDIdx(@Param("dIdx") Integer dIdx);
 }
