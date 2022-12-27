b67b94d7533feead2ee5fee66f3766642a01a2d5
HyeyeonJ <michaelvia29@gmail.com>
2022-12-27 15:52:34 +0900

    카카오 api 기본정보 DB insert

--- src/main/java/project/carPooling/driver/domain/DriverInfo.java
+++ src/main/java/project/carPooling/driver/domain/DriverInfo.java
@@ -8,16 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DriverInfo {
-<<<<<<< HEAD
 	
 	private int dIdx;
 	private UserType dUserType;
 	
-=======
-	private Integer dIdx;
-	private UserType dUserType;
-
->>>>>>> ff182bcc64c84c90163adb31c9eb3075dfb5dc7c
 	private String dUserId;
 	private String dUserPw;
 	private String dUserName;
@@ -28,21 +22,12 @@ public class DriverInfo {
 	private String dUserVcode;
 	private String dIdNum1;
 	private String dIdNum2;
-<<<<<<< HEAD
 	
 	private Boolean dSignOut;
 	
-=======
-
-	private Boolean dSignOut;
-
->>>>>>> ff182bcc64c84c90163adb31c9eb3075dfb5dc7c
 	private String dLicenseNum;
 	private String dLicenseDate;
 	private String dCarNum;
 	private CarType dCarType;
-<<<<<<< HEAD
 
-=======
->>>>>>> ff182bcc64c84c90163adb31c9eb3075dfb5dc7c
 }
