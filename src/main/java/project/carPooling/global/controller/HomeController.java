4ab3a4be7d4a38f509ae7339b10b0b7d8206afd7
HyeyeonJ <michaelvia29@gmail.com>
2022-12-28 15:35:20 +0900

    세션 처리

--- src/main/java/project/carPooling/global/controller/HomeController.java
+++ src/main/java/project/carPooling/global/controller/HomeController.java
@@ -1,20 +1,55 @@
 package project.carPooling.global.controller;
 
+import java.util.Enumeration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import project.carPooling.driver.domain.DriverInfo;
+import project.carPooling.global.session.SessionManager;
+import project.carPooling.global.session.SessionVar;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
+	
+	private final SessionManager sessionManager;
+	
 	@GetMapping("/")
-	public String login(Model model) {
+	public String carPoolingHome(Model model, HttpServletRequest req) {
 
-		return "home";
+		HttpSession session = req.getSession();
+		if (session == null) {
+			return "home";
+		}
+		
+		Enumeration<String> sessionName = session.getAttributeNames();
+		while(sessionName.hasMoreElements()) {
+			String name = sessionName.nextElement();
+			log.info("session {}, {}", name, session.getAttribute(name));
+		}
+		
+		log.info("{},{},{},{},{}"
+					, session.getId()
+					, session.getMaxInactiveInterval()
+					, session.getCreationTime()
+					, session.getLastAccessedTime()
+					, session.isNew());
+		
+		
+		DriverInfo driverInfo = (DriverInfo) session.getAttribute(SessionVar.LOGIN_DRIVER);
+
+		if (driverInfo == null) {
+			return "home";
+		}
+		model.addAttribute("driverInfo", driverInfo);
+		return "driver/dRegistration";
 	}
 }
