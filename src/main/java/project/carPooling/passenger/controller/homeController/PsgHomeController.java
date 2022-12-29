60dcf5400c7304b97587306f01c63f6a6ef6be21
DYon <dancingfrog.lee@gmail.com>
2022-12-29 16:40:28 +0900

    passenger-일반로그인 완료

--- src/main/java/project/carPooling/passenger/controller/homeController/PsgHomeController.java
+++ src/main/java/project/carPooling/passenger/controller/homeController/PsgHomeController.java
@@ -25,27 +25,16 @@ public class PsgHomeController {
 					, HttpServletRequest req) {
 		
 		HttpSession session = req.getSession(false);
-		if(session == null) { return "passenger/login/pLoginMain"; }
 		
-		//이 위치에 session이 존재함
-//		for(String sName : session.getAttributeNames()..asIterator())
-		Enumeration<String> sessionName = session.getAttributeNames();
-		while(sessionName.hasMoreElements()) {
-			String name = sessionName.nextElement();
-			log.info("session {}, {}", name, session.getAttribute(name));
-		}
-		
-		log.info("{}, {}, {}, {}, {}"
-				, session.getId()
-				, session.getMaxInactiveInterval()
-				, session.getCreationTime()
-				, session.getLastAccessedTime()
-				, session.isNew());
-		
+		if(session == null || session.getAttribute(SessionVar.LOGIN_PASSENGER) == null) {
+			
+			return "redirect:/passenger/login"; }
+
+
 		//sessionManager에서 넘어온 Object가 driver 혹은 passenger인걸 알 수 있으므로 강제 형변환해주기
 		PassengerInfo passenger = (PassengerInfo)session.getAttribute(SessionVar.LOGIN_PASSENGER);
 		
-		if(passenger == null) { return "passenger/login/pLoginMain"; }
+		if(passenger == null) { return "redirect:/passenger/login"; }
 		
 		model.addAttribute("passenger", passenger);
 		
