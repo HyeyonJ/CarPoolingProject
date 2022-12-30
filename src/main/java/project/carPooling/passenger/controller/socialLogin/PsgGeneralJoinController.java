0812ef683826400d34375a678e02fa0eff03d8e3
DYon <dancingfrog.lee@gmail.com>
2022-12-30 11:34:54 +0900

    HY's genaralJoinController

--- src/main/java/project/carPooling/passenger/controller/socialLogin/PsgGeneralJoinController.java
+++ src/main/java/project/carPooling/passenger/controller/socialLogin/PsgGeneralJoinController.java
@@ -1,4 +1,4 @@
-package project.carPooling.driver.controller.socialLogin;
+package project.carPooling.passenger.controller.socialLogin;
 
 import java.util.Map;
 
@@ -10,19 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import project.carPooling.driver.domain.DriverInfo;
-import project.carPooling.driver.repository.DriverInfoRepository;
-import project.carPooling.driver.validation.DriverValidator;
+import project.carPooling.passenger.repository.PassengerInfoRepository;
+import project.carPooling.passenger.validation.PassengerValidator;
 
 @Slf4j
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/driver/join")
-public class DrvGeneralJoinController {
+@RequestMapping("/passenger/join")
+public class PsgGeneralJoinController {
 	
-	private Map<String, DriverInfo> driverMap;
+	private Map<String, DriverInfo> passengerMap;
 	
-	private final DriverInfoRepository driverInfoRepository;
-	private final DriverValidator driverValidator;
+	private final PassengerInfoRepository passengerInfoRepository;
+	private final PassengerValidator passengerValidator;
 	
 //	@GetMapping("/add")
 //	public String JoinAdd(Model model) {
