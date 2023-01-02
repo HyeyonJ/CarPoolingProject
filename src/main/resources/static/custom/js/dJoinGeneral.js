fa6b3d971d8aa860e3173a1edbd6f38befba0fc8
HyeyeonJ <michaelvia29@gmail.com>
2023-01-02 18:07:12 +0900

    google 로그인 api 환경 설정

--- src/main/resources/static/custom/js/dJoinGeneral.js
+++ src/main/resources/static/custom/js/dJoinGeneral.js
@@ -13,7 +13,7 @@ window.addEventListener('load', () => {
     const forms = document.getElementsByClassName('validation-form');
 
     Array.prototype.filter.call(forms, (form) => {
-	    form.addEventListener('submit', function (event) {
+	    form.addEventListener('button', function (event) {
 	    	if (form.checkValidity() === false) {
 	        	event.preventDefault();
 	            event.stopPropagation();
