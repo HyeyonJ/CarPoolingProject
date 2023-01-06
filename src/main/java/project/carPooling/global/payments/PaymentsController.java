74852ea8187e270892582e6dd535b30a024e4366
HyeyeonJ <michaelvia29@gmail.com>
2023-01-06 11:21:05 +0900

    Driver naver API socialId 까지 DB Insert됨

--- src/main/java/project/carPooling/global/payments/PaymentsController.java
+++ src/main/java/project/carPooling/global/payments/PaymentsController.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.siot.IamportRestClient.exception.IamportResponseException;
-import com.siot.IamportRestClient.response.IamportResponse;
-import com.siot.IamportRestClient.response.Payment;
+//import com.siot.IamportRestClient.exception.IamportResponseException;
+//import com.siot.IamportRestClient.response.IamportResponse;
+//import com.siot.IamportRestClient.response.Payment;
 
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -24,23 +24,23 @@ import lombok.extern.slf4j.Slf4j;
 public class PaymentsController {
 
 	
-	@GetMapping("/mobile/complete")
-	public String orderCompleteMobile(
-			@RequestParam(required = false) String imp_uid
-			, @RequestParam(required = false) String merchant_uid
-			, Model model
-			, Locale locale
-			, HttpSession session) throws IamportResponseException, IOException
-	{
-		
-		IamportResponse<Payment> result = api.paymentByImpUid(imp_uid);
-		
-		// 결제 가격과 검증결과를 비교한다.
-		if(result.getResponse().getAmount().compareTo(BigDecimal.valueOf(100)) == 0) {
-			System.out.println("검증통과");
-		}
-		
-		return "home";
-	}
+//	@GetMapping("/mobile/complete")
+//	public String orderCompleteMobile(
+//			@RequestParam(required = false) String imp_uid
+//			, @RequestParam(required = false) String merchant_uid
+//			, Model model
+//			, Locale locale
+//			, HttpSession session) throws IamportResponseException, IOException
+//	{
+//		
+//		IamportResponse<Payment> result = api.paymentByImpUid(imp_uid);
+//		
+//		// 결제 가격과 검증결과를 비교한다.
+//		if(result.getResponse().getAmount().compareTo(BigDecimal.valueOf(100)) == 0) {
+//			System.out.println("검증통과");
+//		}
+//		
+//		return "home";
+//	}
 
 }
